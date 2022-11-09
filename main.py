f0dd25a20973135956c30d32f86adfb107f07d89
Waqas <waqasbinhamed@gmail.com>
2022-11-09 16:47:25 -0500

    modified code, added new notebook and real life dataset.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+EPS = 1e-16
+THRES = 1e-10
+
 
 def initialize_matrices(m, n, r):
     return np.random.rand(m, r), np.random.rand(r, n)
@@ -10,13 +13,13 @@ def non_neg(arr):
     return arr
 
 
-def line_search(c, diff_wis, hj_norm_sq, MhT, _lambda, grad, beta=0.75, itermax=1000, backtracking=False):
+def line_search(c, diff_wis, hj_norm_sq, MhT, _lambda, grad, beta=0.75, itermax=1000):
     t = 1
     k = 0
     val = func(c, diff_wis, hj_norm_sq, MhT, _lambda)
-    # BACKTRACKING DOES NOT WORK
-    if backtracking:
-        val = val - 0.5 * t * np.linalg.norm(grad) ** 2
+    # # BACKTRACKING DOES NOT WORK
+    # if backtracking:
+    #     val = val - 0.5 * t * np.linalg.norm(grad) ** 2
     while k < itermax and func(c - t * grad, diff_wis, hj_norm_sq, MhT, _lambda) > val:
         t *= beta
         k += 1
@@ -53,7 +56,7 @@ def update_wj_1(W, Mj, c, hj, j, m, _lambda, itermax=1000):
         t = line_search(c, diff_wis_norm, hj_norm_sq, MhT, _lambda, grad)
         new_c = non_neg(c - t * grad)
 
-        if np.linalg.norm(c - new_c) / np.linalg.norm(c) < 1e-8:
+        if np.linalg.norm(c - new_c) / np.linalg.norm(c) < THRES:
             break
     return new_c
 
@@ -75,7 +78,7 @@ def update_wj_2(W, Mj, new_z, hj, j, m, r, _lambda, rho=0.75, itermax=1000):
         new_w0 = non_neg(z - y0 / rho)
 
         zeta_arr = z - yi_arr / rho
-        tmp_arr = zeta_arr / (_lambda + 1e-12) - ci_arr
+        tmp_arr = zeta_arr / (_lambda + EPS) - ci_arr
 
         tmp_norm = np.linalg.norm(tmp_arr, axis=0)
         norm_mask = tmp_norm > 1
@@ -90,7 +93,7 @@ def update_wj_2(W, Mj, new_z, hj, j, m, r, _lambda, rho=0.75, itermax=1000):
         new_y0 = y0 + rho * (new_w0 - new_z)
         new_yi_arr = yi_arr + rho * (new_wi_arr - new_z)
 
-        if np.linalg.norm(z - new_z) / np.linalg.norm(z) < 1e-8:
+        if np.linalg.norm(z - new_z) / np.linalg.norm(z) < THRES:
             break
     return new_z
 
@@ -114,18 +117,26 @@ def update_wj_3(new_wj, hj, Mj, W, j, m, _lambda, mu=1, itermax=1000):
 
         new_wj = non_neg(wj - t * grad)
 
-        if np.linalg.norm(wj - new_wj) / np.linalg.norm(wj) < 1e-8:
+        if np.linalg.norm(wj - new_wj) / np.linalg.norm(wj) < THRES:
             break
     return new_wj
 
 
-def nmf(M, W, H, _lambda=0.0, itermax=1000, update_ver=2):
-    scores = np.empty((1, itermax))
-    scores[:] = np.NaN
-
+def nmf(M, W, H, _lambda=0.0, itermax=1000, update_ver=2, scaled=False, gswitch=False):
     m, n = M.shape
     r = W.shape[1]
 
+    if scaled:
+        g_ini = 0
+        for i in range(r):
+            g_ini += np.sum(np.linalg.norm(W[:, i: i + 1] - W[:, i + 1:], axis=0))
+        scaled_lambda = (np.linalg.norm(M - W @ H, 'fro') / g_ini) * _lambda
+    else:
+        scaled_lambda = _lambda
+    under_theshold_flag = False
+
+    fscores = np.zeros((itermax, 1))
+    gscores = np.zeros((itermax, 1))
     best_score = np.Inf
     W_best = np.empty((m, r))
     H_best = np.empty((r, n))
@@ -139,44 +150,56 @@ def nmf(M, W, H, _lambda=0.0, itermax=1000, update_ver=2):
             Mj = Mj + wj @ hj
 
             # update h_j
-            H[j: j + 1, :] = hj = non_neg(wj.T @ Mj) / (np.linalg.norm(wj) ** 2)
+            H[j: j + 1, :] = hj = non_neg(wj.T @ Mj) / (np.linalg.norm(wj) ** 2 + EPS)
 
             # update w_j
 
             if update_ver == 1:
-                W[:, j: j + 1] = wj = update_wj_1(W, Mj, wj, hj, j, m, _lambda)
+                W[:, j: j + 1] = wj = update_wj_1(W, Mj, wj, hj, j, m, scaled_lambda)
             elif update_ver == 2:
-                W[:, j: j + 1] = wj = update_wj_2(W, Mj, wj, hj, j, m, r, _lambda)
+                W[:, j: j + 1] = wj = update_wj_2(W, Mj, wj, hj, j, m, r, scaled_lambda)
             elif update_ver == 3:
-                W[:, j: j + 1] = wj = update_wj_3(wj, hj, Mj, W, j, m, _lambda)
-
+                W[:, j: j + 1] = wj = update_wj_3(wj, hj, Mj, W, j, m, scaled_lambda)
             Mj = Mj - wj @ hj
-        scores[0, it] = np.linalg.norm(M - W @ H, 'fro')
-        if scores[0, it] > best_score:
-            best_score = scores[0, it]
+
+        fscores[it] = np.linalg.norm(M - W @ H, 'fro')
+        for i in range(r):
+            gscores[it] += np.sum(np.linalg.norm(W[:, i: i+1] - W[:, i+1:], axis=0))
+        total_score = fscores[it] + scaled_lambda * gscores[it]
+        if total_score > best_score:
+            best_score = total_score
             W_best = W
             H_best = H
-        print(it, scores[0, it])
-    return W_best, H_best, scores
+        print(f'Iteration: {it + 1}, f={fscores[it]}, g={gscores[it]},  total={total_score}')
+
+        if gswitch:
+            if gscores[it] < 0.1 and not under_theshold_flag:
+                scaled_lambda = 0.1 * scaled_lambda
+                under_theshold_flag = True
+            if under_theshold_flag and gscores[it] > 0.1:
+                scaled_lambda = 10 * scaled_lambda
+                under_theshold_flag = False
+
+    return W_best, H_best, W, H, fscores, gscores
 
 
-if __name__ == '__main__':
-    np.random.seed(42)
-
-    def zeros_mask(arr):
-        m, n = arr.shape
-        indices = np.random.choice(m * n, replace=False, size=int(m * n * 0.4))
-        arr[np.unravel_index(indices, (m, n))] = 0
-        return arr
-
-
-    m, n, r_true = 8, 5, 3
-    W_true = zeros_mask(np.random.rand(m, r_true))
-    H_true = zeros_mask(np.random.rand(r_true, n))
-    M = W_true @ H_true
-
-    r = 5
-    W_ini = np.random.rand(m, r)
-    H_ini = np.random.rand(r, n)
-    W, H, scores = nmf(M, W_ini, H_ini, _lambda=0.4, itermax=100, update_ver=2)
-    print('done')
+# if __name__ == '__main__':
+#     np.random.seed(42)
+#
+#     def zeros_mask(arr):
+#         m, n = arr.shape
+#         indices = np.random.choice(m * n, replace=False, size=int(m * n * 0.4))
+#         arr[np.unravel_index(indices, (m, n))] = 0
+#         return arr
+#
+#
+#     m, n, r_true = 8, 5, 3
+#     W_true = zeros_mask(np.random.rand(m, r_true))
+#     H_true = zeros_mask(np.random.rand(r_true, n))
+#     M = W_true @ H_true
+#
+#     r = 5
+#     W_ini = np.random.rand(m, r)
+#     H_ini = np.random.rand(r, n)
+#     W, H, fscores, gscores = nmf(M, W_ini, H_ini, _lambda=0.4, itermax=100, update_ver=2)
+#     print('done')
