1a132a21080b07643b065ef504954294cee3e2f2
Waqas <waqasbinhamed@gmail.com>
2022-11-15 12:53:23 -0500

    added new notebook, modified code to make it simpler

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def update_wj_1(W, Mj, c, hj, j, m, _lambda, itermax=1000):
         tmp = diff_wis.copy()
         tmp[:, norm_mask] = diff_wis[:, norm_mask] / diff_wis_norm[norm_mask]
         tmp[:, ~norm_mask] = unit_norm_vec(m)
-        grad = hj_norm_sq * c - MhT + np.sum(tmp, axis=1).reshape(m, 1)
+        grad = hj_norm_sq * c - MhT + np.sum(tmp, axis=1, keepdims=True)
 
         # simple line search
         t = line_search(c, diff_wis_norm, hj_norm_sq, MhT, _lambda, grad)
@@ -60,11 +60,12 @@ def update_wj_1(W, Mj, c, hj, j, m, _lambda, itermax=1000):
     return new_c
 
 
-def update_wj_2(W, Mj, new_z, hj, j, m, r, _lambda, rho=0.75, itermax=1000):
+def update_wj_2(W, Mj, new_z, hj, j, m, r, _lambda, rho=0.75, itermax=1000, zero_thres=False):
     num_edges = r * (r - 1)
     ci_arr = np.delete(W, j, axis=1)
     new_wi_arr = new_yi_arr = np.random.rand(m, r - 1)
-    new_yf = new_y0 = np.random.rand(m, 1)
+    new_yf = np.random.rand(m, 1)
+    new_y0 = np.random.rand(m, 1)
 
     hj_norm_sq = np.linalg.norm(hj) ** 2
     for it in range(itermax):
@@ -73,7 +74,9 @@ def update_wj_2(W, Mj, new_z, hj, j, m, r, _lambda, rho=0.75, itermax=1000):
         y0 = new_y0
         yi_arr = new_yi_arr
 
-        new_wf = non_neg((Mj @ hj.T - yf + rho * z) / (rho + hj_norm_sq))
+        new_wf = (Mj @ hj.T - yf + rho * z) / (rho + hj_norm_sq)
+        if zero_thres:
+            new_wf = non_neg(new_wf)
         new_w0 = non_neg(z - y0 / rho)
 
         zeta_arr = z - yi_arr / rho
@@ -83,10 +86,9 @@ def update_wj_2(W, Mj, new_z, hj, j, m, r, _lambda, rho=0.75, itermax=1000):
         norm_mask = tmp_norm > 1
         new_wi_arr[:, norm_mask] = zeta_arr[:, norm_mask] - _lambda * (tmp_arr[:, norm_mask] / tmp_norm[norm_mask])
         new_wi_arr[:, ~norm_mask] = zeta_arr[:, ~norm_mask] - _lambda * tmp_arr[:, ~norm_mask]
-        new_wi_arr = non_neg(new_wi_arr)
-        new_z = (rho * (new_wf + new_w0) + rho * np.sum(new_wi_arr, axis=1).reshape(m, 1) + yf + y0 + np.sum(yi_arr,
-                                                                                                             axis=1).reshape(
-            m, 1)) / (rho * (2 + num_edges))
+        if zero_thres:
+            new_wi_arr = non_neg(new_wi_arr)
+        new_z = (rho * (new_wf + new_w0) + rho * np.sum(new_wi_arr, axis=1, keepdims=True) + yf + y0 + np.sum(yi_arr, axis=1, keepdims=True)) / (rho * (2 + num_edges))
 
         new_yf = yf + rho * (new_wf - new_z)
         new_y0 = y0 + rho * (new_w0 - new_z)
@@ -111,7 +113,7 @@ def update_wj_3(new_wj, hj, Mj, W, j, m, _lambda, mu=1, itermax=1000):
         norm_mask = tmp_norm > 1
         tmp_arr[:, norm_mask] = tmp_arr[:, norm_mask] / tmp_norm[norm_mask]
 
-        grad = hj_norm_sq * wj - MhT + np.sum(tmp_arr, axis=1).reshape(m, 1)
+        grad = hj_norm_sq * wj - MhT + np.sum(tmp_arr, axis=1, keepdims=True)
         t = line_search(wj, wj - wi_arr, hj_norm_sq, MhT, _lambda, grad)
 
         new_wj = non_neg(wj - t * grad)
@@ -121,28 +123,33 @@ def update_wj_3(new_wj, hj, Mj, W, j, m, _lambda, mu=1, itermax=1000):
     return new_wj
 
 
-def nmf(M, W, H, _lambda=0.0, itermax=1000, update_ver=2, scale_lambda=False, gswitch=False, verbose=False):
+def nmf(M, W, H, _lambda=0.0, itermax=1000, update_ver=2, scale_lambda=False, zero_thres=False, gswitch=False, verbose=False):
     m, n = M.shape
     r = W.shape[1]
 
-    if scale_lambda:
-        g_ini = 0
-        for i in range(r):
-            g_ini += np.sum(np.linalg.norm(W[:, i: i + 1] - W[:, i + 1:], axis=0))
-        scaled_lambda = (np.linalg.norm(M - W @ H, 'fro') / g_ini) * _lambda
-    else:
-        scaled_lambda = _lambda
-
     under_theshold_flag = False
 
-    fscores = np.zeros((itermax,))
-    gscores = np.zeros((itermax,))
+    fscores = np.zeros((itermax + 1,))
+    gscores = np.zeros((itermax + 1,))
+    lambda_vals = np.zeros((itermax + 1,))
+
+    fscores[0] = np.linalg.norm(M - W @ H, 'fro')
+    for i in range(r):
+        gscores[0] += np.sum(np.linalg.norm(W[:, i: i + 1] - W[:, i + 1:], axis=0))
+    if scale_lambda:
+        scaled_lambda = lambda_vals[0] = (fscores[0] / gscores[0]) * _lambda
+    else:
+        scaled_lambda = _lambda
+        lambda_vals[:] = _lambda
+
     best_score = np.Inf
     W_best = np.empty((m, r))
     H_best = np.empty((r, n))
 
     Mj = M - W @ H
-    for it in range(itermax):
+    it = 1
+    # while it <= itermax or (W < 0).any():
+    for it in range(1, itermax + 1):
         for j in range(r):
             wj = W[:, j: j + 1]
             hj = H[j: j + 1, :]
@@ -157,7 +164,7 @@ def nmf(M, W, H, _lambda=0.0, itermax=1000, update_ver=2, scale_lambda=False, gs
             if update_ver == 1:
                 W[:, j: j + 1] = wj = update_wj_1(W, Mj, wj, hj, j, m, scaled_lambda)
             elif update_ver == 2:
-                W[:, j: j + 1] = wj = update_wj_2(W, Mj, wj, hj, j, m, r, scaled_lambda)
+                W[:, j: j + 1] = wj = update_wj_2(W, Mj, wj, hj, j, m, r, scaled_lambda, zero_thres=zero_thres)
             elif update_ver == 3:
                 W[:, j: j + 1] = wj = update_wj_3(wj, hj, Mj, W, j, m, scaled_lambda)
             Mj = Mj - wj @ hj
@@ -171,18 +178,25 @@ def nmf(M, W, H, _lambda=0.0, itermax=1000, update_ver=2, scale_lambda=False, gs
             W_best = W
             H_best = H
 
+        if scale_lambda:
+            scaled_lambda = lambda_vals[it] = (fscores[it] / gscores[it]) * _lambda
+
         if verbose:
-            print(f'Iteration: {it + 1}, f={fscores[it]}, g={gscores[it]},  total={total_score}')
+            print(f'Iteration: {it}, f={fscores[it]}, g={gscores[it]},  total={total_score}, {(W >= 0).all()}')
+
+        # it += 1
+
 
         if gswitch:
-            if gscores[it] < 0.1 and not under_theshold_flag:
-                scaled_lambda = 0.1 * scaled_lambda
+            if gscores[it] / fscores[it] < 1 and not under_theshold_flag:
+                _lambda = 0.1 * _lambda
                 under_theshold_flag = True
-            if under_theshold_flag and gscores[it] > 0.1:
-                scaled_lambda = 10 * scaled_lambda
+            if under_theshold_flag and gscores[it] / fscores[it] > 1:
+                _lambda = 10 * _lambda
                 under_theshold_flag = False
 
-    return W_best, H_best, W, H, fscores, gscores
+    return W_best, H_best, W, H, fscores, gscores, lambda_vals
+
 
 if __name__ == '__main__':
     np.random.seed(42)
---
+++ simple.py
@@ -0,0 +1,103 @@
+import numpy as np
+
+
+def non_neg(arr):
+    arr[arr < 0] = 0
+    return arr
+
+
+def calculate_gscore(W, r):
+    gscore = 0
+    for i in range(r):
+        gscore += np.sum(np.linalg.norm(W[:, i: i + 1] - W[:, i + 1:], axis=0))
+    return gscore
+
+
+def update_wj(W, Mj, new_z, hj, j, m, r, _lambda, rho=0.5, itermax=1000):
+    num_edges = r * (r - 1)
+    ci_arr = np.delete(W, j, axis=1)
+    
+    new_wi_arr = np.zeros((m, r - 1))
+    
+    new_yi_arr = np.random.rand(m, r - 1)
+    new_yf = np.random.rand(m, 1)
+    new_y0 = np.random.rand(m, 1)
+
+    hj_norm_sq = np.linalg.norm(hj) ** 2
+    for it in range(itermax):
+        z = new_z
+        yf = new_yf
+        y0 = new_y0
+        yi_arr = new_yi_arr
+
+        new_wf = (Mj @ hj.T - yf + rho * z) / (rho + hj_norm_sq)
+        new_w0 = non_neg(z - y0 / rho)
+
+        zeta_arr = z - yi_arr / rho
+        tmp_arr = zeta_arr / _lambda - ci_arr
+
+        tmp_norm = np.linalg.norm(tmp_arr, axis=0)
+        norm_mask = tmp_norm > 1
+        new_wi_arr[:, norm_mask] = zeta_arr[:, norm_mask] - _lambda * (tmp_arr[:, norm_mask] / tmp_norm[norm_mask])
+        new_wi_arr[:, ~norm_mask] = zeta_arr[:, ~norm_mask] - _lambda * tmp_arr[:, ~norm_mask]
+
+        new_z = (rho * (new_wf + new_w0) + rho * np.sum(new_wi_arr, axis=1, keepdims=True) + yf + y0 + np.sum(yi_arr, axis=1, keepdims=True)) / (rho * (2 + num_edges))
+
+        new_yf = yf + rho * (new_wf - new_z)
+        new_y0 = y0 + rho * (new_w0 - new_z)
+        new_yi_arr = yi_arr + rho * (new_wi_arr - new_z)
+    return new_z
+
+
+def nmf_son(M, W, H, _lambda=0.0, itermax=1000, scale_lambda=False, verbose=False):
+    m, n = M.shape
+    r = W.shape[1]
+
+    fscores = np.zeros((itermax + 1,))
+    gscores = np.zeros((itermax + 1,))
+    lambda_vals = np.zeros((itermax + 1,))
+
+    fscores[0] = np.linalg.norm(M - W @ H, 'fro')
+    gscores[0] = calculate_gscore(W, r)
+
+    if scale_lambda:
+        scaled_lambda = lambda_vals[0] = (fscores[0] / gscores[0]) * _lambda
+    else:
+        scaled_lambda = _lambda
+        lambda_vals[:] = _lambda
+
+    best_score = np.Inf
+    W_best = np.zeros((m, r))
+    H_best = np.zeros((r, n))
+
+    Mj = M - W @ H
+    for it in range(1, itermax + 1):
+        for j in range(r):
+            wj = W[:, j: j + 1]
+            hj = H[j: j + 1, :]
+
+            Mj = Mj + wj @ hj
+
+            # update h_j
+            H[j: j + 1, :] = hj = non_neg(wj.T @ Mj) / (np.linalg.norm(wj) ** 2)
+
+            # update w_j
+            W[:, j: j + 1] = wj = update_wj(W, Mj, wj, hj, j, m, r, scaled_lambda)
+
+            Mj = Mj - wj @ hj
+
+        fscores[it] = np.linalg.norm(M - W @ H, 'fro')
+        gscores[it] = calculate_gscore(W, r)
+        total_score = fscores[it] + scaled_lambda * gscores[it]
+
+        if total_score > best_score:
+            best_score = total_score
+            W_best = W
+            H_best = H
+
+        if scale_lambda:
+            scaled_lambda = lambda_vals[it] = (fscores[it] / gscores[it]) * _lambda
+
+        if verbose:
+            print(f'Iteration: {it}, f={fscores[it]}, g={gscores[it]},  total={total_score}')
+    return W_best, H_best, W, H, fscores, gscores, lambda_vals
